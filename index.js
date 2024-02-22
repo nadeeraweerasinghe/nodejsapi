a5d156736a1ec4f2eb1cc0335b3af1e464ba1d6a
Nadeera Weerasinghe Arachchige <nadeera.weerasinghe@pearson.com>
2024-02-22 13:48:38 +1100

    hosting local setup under domain

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var cors = require('cors');
 
 const app = express();
 const port = 3000;
-var allowedOrigins = ['http://localhost:4200',
+var allowedOrigins = ['http://local.poc.io',
                       'https://nadeeraweerasinghe.github.io/ngwebsite2/'];app.use(cors({
     credentials: true,
   origin: function(origin, callback){    // allow requests with no origin 
