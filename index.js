10c24b2f1b3f3869f7ebb5abd361cc81a6cf3d19
Nadeera Weerasinghe Arachchige <nadeera.weerasinghe@pearson.com>
2024-02-27 13:28:35 +1100

    update origin

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var cors = require('cors');
 const app = express();
 const port = 3000;
 var allowedOrigins = ['https://local.poc.io',
-                      'https://nadeeraweerasinghe.github.io/ngwebsite2/'];app.use(cors({
+                      'https://nadeeraweerasinghe.github.io'];app.use(cors({
     credentials: true,
   origin: function(origin, callback){    // allow requests with no origin 
     // (like mobile apps or curl requests)
