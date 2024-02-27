008b3d298665d98b0100bcd3d552a97703e41d53
Nadeera Weerasinghe Arachchige <nadeera.weerasinghe@pearson.com>
2024-02-27 13:36:37 +1100

    add logs

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var allowedOrigins = ['https://local.poc.io',
     // (like mobile apps or curl requests)
     if(!origin) return callback(null, true);    if(allowedOrigins.indexOf(origin) === -1){
       var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
+                'allow access from the specified Origin- ' + origin;
       return callback(new Error(msg), false);
     }    return callback(null, true);
   }
